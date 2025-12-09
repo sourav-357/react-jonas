7fe7fefbb2fc490487e8419ae49868436e942622
Sourav Kumar <xsouravkumar357@gmail.com>
2025-12-09 06:57:41 +0530

    First Code

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -1,1 +1,10 @@
+import React from "react";
+import ReactDom from "react-dom/client";
 
+function App() {
+  return <h1>Hello React!</h1>;
+}
+
+// react version 18
+const root = ReactDom.createRoot(document.getElementById("root"));
+root.render(<App />);
