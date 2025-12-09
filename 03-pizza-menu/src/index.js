c1063f917d5e0e5e1f89a506cc65063dfdf98ab7
Sourav Kumar <xsouravkumar357@gmail.com>
2025-12-09 15:42:22 +0530

    react fragments

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -73,12 +73,20 @@ function Menu() {
   return (
     <main className="menu">
       <h2> Our Menu </h2>
+
       {numPizza > 0 ? (
-        <ul className="pizzas">
-          {pizzaData.map((pizza) => (
-            <Pizza pizzObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delecious.
+          </p>
+
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza pizzObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We are still working on our menu. Please come back later.</p>
       )}
