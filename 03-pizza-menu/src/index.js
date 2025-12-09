f12d2f8fc1eca582393dc0e85b12876355769a6b
Sourav Kumar <xsouravkumar357@gmail.com>
2025-12-09 07:56:09 +0530

    importing css

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom/client";
+import "./index.css";
 
 const pizzaData = [
   {
@@ -59,7 +60,8 @@ function App() {
 
 // creating the header component
 function Header() {
-  return <h1>Fast React Pizza Co.</h1>;
+  const style = { color: "red", fontSize: "48px", textTransform: "uppercase" };
+  return <h1 style={style}>Fast React Pizza Co.</h1>;
 }
 
 // creating the Menu component
