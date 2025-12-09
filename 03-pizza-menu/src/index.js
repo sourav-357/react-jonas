9a41eb379f59b9648a814f8038f3d46b93aec97d
Sourav Kumar <xsouravkumar357@gmail.com>
2025-12-09 07:10:48 +0530

    Folder structure balanced

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -7,4 +7,8 @@ function App() {
 
 // react version 18
 const root = ReactDom.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
