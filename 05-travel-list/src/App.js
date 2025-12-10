b865d4cdeb1256cd38575fa85814a7815e134474
Sourav Kumar <xsouravkumar357@gmail.com>
2025-12-10 17:21:49 +0530

    Basic layout of the project

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+export default function App() {
+  return <div className="app">
+    <Logo />
+    <Form />
+    <PackingList />
+    <Stats />
+  </div>
+}
 
-function App() {
+function Logo() {
+  return <h1>🌴 Far away 🎒</h1>;
+}
+
+function Form() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
     </div>
   );
 }
 
-export default App;
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+
+function Stats() {
+  <footer>
+    <em>🎒 You have X items in the list, and you already packed X (X%)</em>
+  </footer>;
+}
