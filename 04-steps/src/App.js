009d2e5beadb6c02e36ef60d2b78a7bddef2d988
Sourav Kumar <xsouravkumar357@gmail.com>
2025-12-10 06:40:29 +0530

    completed the static architecture

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -1,25 +1,21 @@
-import logo from "./logo.svg";
-import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className="steps">
+      <div className="numbers">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
 
-export default App;
+      <p className="message">
+        Hello there !!
+      </p>
+
+      <div className="buttons">
+        <button>Next</button>
+        <button>previous</button>
+      </div>
+    </div>
+  )
+}
