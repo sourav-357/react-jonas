ace497e8bfeb44e7c919a9bd539ab8a304f8b5f0
Sourav Kumar <xsouravkumar357@gmail.com>
2025-12-10 06:52:32 +0530

    Built the static part of project

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -1,21 +1,32 @@
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
 
 export default function App() {
+  const step = 3;
+
   return (
     <div className="steps">
       <div className="numbers">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
+        <div className={`${step >= 1 ? "active" : ""} `}>1</div>
+        <div className={`${step >= 2 ? "active" : ""} `}>2</div>
+        <div className={`${step >= 3 ? "active" : ""} `}>3</div>
       </div>
 
       <p className="message">
-        Hello there !!
+        Step {step}: {messages[step - 1]}
       </p>
 
       <div className="buttons">
-        <button>Next</button>
-        <button>previous</button>
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          previous
+        </button>
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          next
+        </button>
       </div>
     </div>
-  )
+  );
 }
