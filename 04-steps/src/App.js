6063a0614e3cd5f6886afe8e07e1663b8ec35834
Sourav Kumar <xsouravkumar357@gmail.com>
2025-12-10 16:03:48 +0530

    completed 4th steps project

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -22,7 +22,7 @@ export default function App() {
   return (
     <>
       <button className="close" onClick={() => setIsOpen((is) => !is)}>
-        X
+        (X)
       </button>
 
       {isopen && (
