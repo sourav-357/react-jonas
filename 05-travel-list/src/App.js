02cb7c2553e97d2d440244a715891b73bd0f2a74
Sourav Kumar <xsouravkumar357@gmail.com>
2025-12-10 17:26:01 +0530

    moved the array of items from css to App

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -1,10 +1,18 @@
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 export default function App() {
-  return <div className="app">
-    <Logo />
-    <Form />
-    <PackingList />
-    <Stats />
-  </div>
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
 }
 
 function Logo() {
@@ -24,7 +32,9 @@ function PackingList() {
 }
 
 function Stats() {
-  <footer>
-    <em>🎒 You have X items in the list, and you already packed X (X%)</em>
-  </footer>;
+  return (
+    <footer className="stats">
+      <em>🎒 You have X items in the list, and you already packed X (X%)</em>
+    </footer>
+  );
 }
